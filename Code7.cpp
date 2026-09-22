d8093f7e878c5d8eaf1d2c1275edd07267da7c0f
adityashivale33 <adityashivale33@gmail.com>
2026-09-22 22:43:00 +0530

    Add files via upload

---
+++ Code7.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    static int count;
+
+    Student()
+    {
+        count++;
+    }
+};
+
+int Student::count = 0;
+
+int main()
+{
+    Student s1, s2, s3;
+
+    cout << "Number of Students = " << Student::count << endl;
+
+    return 0;
+}
